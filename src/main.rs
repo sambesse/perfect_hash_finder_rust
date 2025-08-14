7b4f9bd8b1ac42fff06fea6924eebf5fa562c79f
Sam Besse <sam.besse@gmail.com>
2025-08-13 22:30:43 -0400

    fixed it to work with structs

--- src/main.rs
+++ src/main.rs
@@ -28,31 +28,34 @@ fn main() -> () {
     can_ids.push(MessageDesc::new(0x23));
     can_ids.push(MessageDesc::new(0x16));
     can_ids.push(MessageDesc::new(0x203));
-    can_ids.push(MessageDesc::new(0x600));
+    can_ids.push(MessageDesc::new(0x301));
     can_ids.push(MessageDesc::new(0x600));
     can_ids.push(MessageDesc::new(0x601));
     can_ids.push(MessageDesc::new(0x602));
 
     let mut m = can_ids.len() as u32;
     let mut k = 1u32;
+    let mut hash_table: Vec<Option<MessageDesc>> = Vec::new();
+    let mut n = 0;
+    while n < m {
+        hash_table.push(None);
+        n += 1;
+    }
 
     loop {
-        let mut hash_table: Vec<Option<MessageDesc>> = Vec::new();
-        let mut n = 0;
-        while n < m {
-            hash_table.push(None);
-            n += 1;
-        }
         while k < K_MAX {
             let mut i = 0;
             while i < can_ids.len() {
                 let index = hash(m, k, &can_ids[i].id);
                 match hash_table.get(index as usize) {
                     None => {
-                        hash_table[index as usize] = Some(MessageDesc::new(can_ids[i].id));
+                        break;
                         // hash_table.replace(index as usize, Some(MessageDesc::new(can_ids[i].id)))
                     }
-                    Some(_i) => break,
+                    Some(_i) => match _i {
+                        None => hash_table[index as usize] = Some(MessageDesc::new(can_ids[i].id)),
+                        Some(_j) => break,
+                    },
                 };
                 i += 1;
             }
@@ -80,6 +83,7 @@ fn main() -> () {
         }
         k = 0;
         m += 1;
+        hash_table.push(None);
     }
 }
 
